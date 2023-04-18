8c47cc95e456fed9f28f6d24693791f461630431
hrudolf <h.rudolf@gmail.com>
2023-04-18 22:20:56 +0200

    Add EmployeeTypeController

---
+++ backend/Controllers/EmployeeTypeController.cs
@@ -0,0 +1,47 @@
+﻿using backend.Model;
+using backend.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EmployeeTypeController : ControllerBase
+{
+    private readonly IEmployeeTypeService _service;
+
+    public EmployeeTypeController(IEmployeeTypeService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public IActionResult GetAllEmployeeTypes()
+    {
+        return Ok(_service.GetAllEmployeeTypes());
+    }
+    
+    [HttpGet("{id:int}")]
+    public IActionResult GetEmployeeTypeById(int id)
+    {
+        return Ok(_service.GetById(id));
+    }
+    
+    [HttpPost]
+    public IActionResult CreateEmployeeType([FromBody] EmployeeType employeeType)
+    {
+        return Ok(_service.CreateEmployeeType(employeeType));
+    }
+    
+    [HttpDelete("{id:int}")]
+    public IActionResult DeleteEmployeeTypeById(int id)
+    {
+        return Ok(_service.DeleteEmployeeType(id));
+    }
+    
+    [HttpPut]
+    public IActionResult UpdateEmployeeType([FromBody] EmployeeType employeeType)
+    {
+        return Ok(_service.UpdateEmployeeType(employeeType));
+    }
+}

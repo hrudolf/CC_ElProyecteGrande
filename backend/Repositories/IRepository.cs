297a61d169d8d1d9d280926dca2cbf74a4eb54da
Gergely Papp <gregpappdev.gmail.com>
2023-04-18 17:32:42 +0200

    Create IRepository.cs interface as generic interface for basic CRUD operations

---
+++ backend/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace backend.Repositories;
+
+public interface IRepository<T>
+{
+    void AddNewItem();
+    
+    HashSet<T> GetAllItems();
+    
+    T GetItemById(int id);
+
+    void UpdateItemById(int id);
+
+    void DeleteItemById(int id);
+
+}

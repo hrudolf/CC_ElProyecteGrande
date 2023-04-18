157d17f3bf09313140116b1c3cdfe407cf0ac539
hrudolf <h.rudolf@gmail.com>
2023-04-18 21:24:27 +0200

    Add EmployeeType model

---
+++ backend/Model/EmployeeType.cs
@@ -0,0 +1,16 @@
+﻿namespace backend.Model;
+
+public class EmployeeType
+{
+    private static int Counter = 0;
+    public int Id { get; }
+    public string Type { get; set; }
+    public bool IsDeleted { get; set; }
+
+    public EmployeeType(string type)
+    {
+        Id = Counter++;
+        Type = type;
+        IsDeleted = false;
+    }
+}

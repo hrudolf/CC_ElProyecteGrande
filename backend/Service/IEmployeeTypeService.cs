f999b2d57f5c52225cb88f6c461414573bcdceda
hrudolf <h.rudolf@gmail.com>
2023-04-18 22:11:42 +0200

    Create IEmployeeTypeService interface

---
+++ backend/Service/IEmployeeTypeService.cs
@@ -0,0 +1,12 @@
+﻿using backend.Model;
+
+namespace backend.Service;
+
+public interface IEmployeeTypeService
+{
+    IEnumerable<EmployeeType> GetAllEmployeeTypes();
+    EmployeeType GetById(int id);
+    EmployeeType CreateEmployeeType(EmployeeType employee);
+    EmployeeType DeleteEmployeeType(int id);
+    EmployeeType UpdateEmployeeType(EmployeeType employee);
+}

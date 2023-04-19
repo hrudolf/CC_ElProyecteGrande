4200a37f82774c0ec5c2a6d94b30cd0a85984067
Gergely Papp <gregpappdev@gmail.com>
2023-04-19 17:29:18 +0200

    Create Shift Repository

---
+++ backend/Model/Shift.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+
+namespace backend.Model;
+
+public class Shift
+{
+    private static int _shiftIdCounter;
+    public int ShiftId { get; }
+    public string TimeOfShift { get; set; }
+    public int NursesRequiredForShift { get; set; }
+    public double BonusRate { get; set; }
+    public bool _isActive = true;
+
+    public Shift(string timeOfShift, int nursesRequiredForShift, double bonusRate)
+    {
+        ShiftId = _shiftIdCounter++;
+        TimeOfShift = timeOfShift;
+        NursesRequiredForShift = nursesRequiredForShift;
+        BonusRate = bonusRate;  
+    }
+
+    [JsonConstructor]
+    public Shift(int shiftId, string timeOfShift, int nursesRequiredForShift, double bonusRate)
+    {
+        ShiftId = shiftId;
+        TimeOfShift = timeOfShift;
+        NursesRequiredForShift = nursesRequiredForShift;
+        BonusRate = bonusRate;
+    }
+    
+    public bool GetIsActive()
+    {
+        return _isActive;
+    }
+    
+    public void ChangeIsActive()
+    {
+        _isActive = !_isActive;
+    }
+}

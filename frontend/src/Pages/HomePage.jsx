d651afa295712cb179a1735beeab2e8500e383d0
Gergely Papp <gregpappdev.gmail.com>
2023-04-20 23:20:09 +0200

    Redesign HomePage

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,24 +1,20 @@
 import { Outlet } from "react-router-dom";
 
 const HomePage = () => (
-    <div className="Layout">
-        <div>
-            <h1>Lorem ipsum</h1>
-            <p>Lorem ipsum</p>
-            <h2>Lorem ipsum</h2>
-            <p>Lorem ipsum</p>
-            <h3>Lorem ipsum</h3>
-            <p>Lorem ipsum</p>
-            <h4>Lorem ipsum</h4>
-            <p>Lorem ipsum</p>
-            <h5>Lorem ipsum</h5>
-            <p>Lorem ipsum</p>
-            <h6>Lorem ipsum</h6>
-            <p>Lorem ipsum</p>
-        </div>
-        <Outlet />
+  <div className="Layout">
+    <div className="container bg-light my-auto w-50">
+      <img
+        src={require("./img/rooster.png")}
+        alt="Green Rooster"
+        className="rounded mx-auto d-block p-3"
+      />
+      <h1 className="text-center">Green Rooster</h1>
+      <h6 className="text-center p-3">
+        ROSTERING | ATTENDANCE | VACATION REQUEST | PAYROLL FORECAST
+      </h6>
     </div>
+    <Outlet />
+  </div>
 );
-
 
 export default HomePage;

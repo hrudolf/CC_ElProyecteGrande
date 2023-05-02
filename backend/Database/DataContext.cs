d86e8cc57ef60261bef9bd50c81d7da0e75ae981
whitword <gyorgyi.lajterne@gmail.com>
2023-05-02 15:44:08 +0200

    Register Tables to Db context

--- backend/Database/DataContext.cs
+++ backend/Database/DataContext.cs
@@ -7,6 +7,9 @@ public class DataContext : DbContext
 {
     public DbSet<EmployeeType> EmployeeTypes { get; set; }
     public DbSet<Employee> Employees { get; set; }
+    public DbSet<Roster> Rosters { get; set; }
+    public DbSet<Shift> Shifts { get; set; }
+    public DbSet<VacationRequest> VacationRequests { get; set; }
 
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {

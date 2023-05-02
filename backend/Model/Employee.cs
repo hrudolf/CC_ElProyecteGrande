3556abef1f8c7e3b6470845db04d5d25b735d418
hrudolf <h.rudolf@gmail.com>
2023-05-02 16:14:01 +0200

    Add required attributes and nullability to properties in Employee

--- backend/Model/Employee.cs
+++ backend/Model/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
+using Microsoft.Build.Framework;
 
 namespace backend.Model;
 
@@ -7,17 +8,20 @@ public class Employee
 {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set;}
+    [Required]
     public string FirstName { get; set; }
+    [Required]
     public string LastName { get; set; }
+    [Required]
     public DateTime DateOfBirth { get; set; }
-    public Shift PreferredShift { get; set; }
+    public Shift? PreferredShift { get; set; }
     public int WorkingDays { get; set; }
     public int TotalVacationDays { get; set; }
     public List<VacationRequest> VacationRequests { get; set; } = new();
-    public EmployeeType EmployeeType { get; set; }
+    public EmployeeType? EmployeeType { get; set; }
     public bool EmploymentStatus = true;
     public int MonthlyGrossSalary { get; set; }
-    private bool _isActive = true;
+    public bool IsActive { get; private set; } = true;
 
     /*public Employee(string firstName, string lastName , DateTime dateOfBirth, int workingDays, 
         int totalVacationDays, int monthlyGrossSalary, EmployeeType employeeType, int preferredShift)
@@ -49,11 +53,11 @@ public class Employee
     
     public bool GetIsActive()
     {
-        return _isActive;
+        return IsActive;
     }
     
     public void ChangeIsActive()
     {
-        _isActive = !_isActive;
+        IsActive = !IsActive;
     }
 }

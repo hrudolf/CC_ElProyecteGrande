e39d93ebef32764320ab60f28cc14ae6ee212505
hrudolf <h.rudolf@gmail.com>
2023-05-03 12:43:09 +0200

    Update Shift to use DataContext

---
+++ backend/DTOs/ShiftDto.cs
@@ -0,0 +1,8 @@
+﻿namespace backend.DTOs;
+
+public class ShiftDto
+{
+    public string NameOfShift { get; set; } = String.Empty;
+    public int NursesRequiredForShift { get; set; }
+    public double BonusRate { get; set; }
+}

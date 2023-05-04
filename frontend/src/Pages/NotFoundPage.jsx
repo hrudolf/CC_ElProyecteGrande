9cc8eceb8201ea01d88ad6621cc093e66fc6bfaf
whitword <gyorgyi.lajterne@gmail.com>
2023-05-04 12:34:29 +0200

    Delete Back To homepage link

--- frontend/src/Pages/NotFoundPage.jsx
+++ frontend/src/Pages/NotFoundPage.jsx
@@ -2,10 +2,11 @@ import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
+    <div className="Layout">
     <div id="error-page">
       <h1>Error 404! Oops!</h1>
       <p>The requested site was not found on the server.</p>
-      <Link to={"/"}>Back to homepage</Link>
+    </div>
     </div>
   );
 };

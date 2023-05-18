ec7089ff21b16f685f2ef8c71c66e14488d876fc
hrudolf <h.rudolf@gmail.com>
2023-05-18 12:32:18 +0200

    Remove comments, structure routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,31 @@
+import { useState, createContext, useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./Pages/Layout";
 import NotFoundPage from "./Pages/NotFoundPage";
 import HomePage from "./Pages/HomePage";
-import EmployeeTypes from "./Pages/EmployeeTypes/EmployeeTypes";
-import { Routes, Route, Navigate } from "react-router-dom";
+import About from "./Pages/About";
+import Spinner from "./Pages/Layout/Spinner";
 import Employee from "./Pages/Employee/Employee";
 import CreateEmployee from "./Pages/Employee/CreateEmployee";
 import ModifyEmployee from "./Pages/Employee/ModifyEmployee";
+import EmployeeTypes from "./Pages/EmployeeTypes/EmployeeTypes";
 import VacationRequest from "./Pages/VacationRequests/VacationRequest";
-import VacationRequestPerEmployee from "./Pages/VacationRequests/VacationRequestPerEmployee";
 import CreateVacationRequest from "./Pages/VacationRequests/CreateVacationRequest";
 import ModifyVacationRequest from "./Pages/VacationRequests/ModifyVacationRequest";
+import VacationRequestPerEmployee from "./Pages/VacationRequests/VacationRequestPerEmployee";
 import Shifts from "./Pages/Shift/Shifts";
 import Roster from "./Pages/Roster/Roster";
 import LoginPage from "./Pages/LoginPage";
-import { useState, createContext, useEffect } from "react";
-import Spinner from "./Pages/Layout/Spinner";
 
 export const UserContext = createContext("user");
 
-
-function App() {
+const App = () => {
 
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setLoading(true);
-    /*     setMessage("");
-        setError(""); */
     async function fetchData() {
       const response = await fetch("/login", {
         method: "GET"
@@ -36,12 +34,8 @@ function App() {
       console.log(json)
       if (!response.ok) {
         setLoading(false);
-        /* setError(json.Message);
-        setMessage(''); */
       } else {
         setLoading(false);
-        /* setError('');
-        setMessage('Successful login'); */
         setUser(json);
         console.log(json);
       }
@@ -55,12 +49,16 @@ function App() {
       {!loading && <UserContext.Provider value={{ user, setUser }}>
         <Routes>
           <Route path="/" element={<Layout />}>
+
             <Route path="/" element={<HomePage />}></Route>
+            <Route path="/about" element={<About />}></Route>
+
             <Route path="/employees">
               <Route path="" element={<Employee />} />
               <Route path="create" element={<CreateEmployee />}></Route>
               <Route path="edit/:id" element={<ModifyEmployee />}></Route>
             </Route>
+
             <Route path="/employeetypes" element={<EmployeeTypes />} />
             <Route path="/shifts" element={<Shifts />} />
             <Route path="/roster" element={<Roster />} />
@@ -69,16 +67,19 @@ function App() {
               <Route path="create" element={<CreateVacationRequest />}></Route>
               <Route path="edit/:id" element={<ModifyVacationRequest />}></Route>
             </Route>
+
             <Route path="/vacationrequests/employee">
               <Route path="" element={user !== null ? <VacationRequestPerEmployee /> : <Navigate to="/login"></Navigate>} />
               <Route path="create" element={<CreateVacationRequest />}></Route>
               <Route path="edit/:id" element={<ModifyVacationRequest />}></Route>
             </Route>
+
             <Route path="/login" element={<LoginPage />} />
             <Route path="*" element={<NotFoundPage />}></Route>
           </Route>
         </Routes>
       </UserContext.Provider>}
+
       {loading && <Spinner />}
 
     </div>

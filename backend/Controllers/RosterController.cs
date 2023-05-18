754668a2aa7ce229fb4d6e6c0a5188d1b7a08c7c
Gergely Papp <gregpappdev.gmail.com>
2023-05-18 23:02:41 +0200

    Refactor RosterController.cs to send error message

--- backend/Controllers/RosterController.cs
+++ backend/Controllers/RosterController.cs
@@ -44,10 +44,14 @@ namespace backend.Controllers
 
 
         [HttpPost("GenerateWeeklyRoster")]
-        public List<Roster> GenerateWeeklyRoster([FromBody] DateTime date)
+        public IActionResult GenerateWeeklyRoster([FromBody] DateTime date)
         {
-            _service.GenerateWeeklyRoster(date);
-            return _service.GetAll().ToList();
+            if (_service.GenerateWeeklyRoster(date))
+            {
+                return Ok(_service.GetAll().ToList());
+            }
+
+            return Problem("Please choose a Monday for weekly roster generation");
         }
         
         [HttpPost]

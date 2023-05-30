3ae59df298ac57cff29182f27e49ba413f23e5bf
Gergely Papp <gregpappdev.gmail.com>
2023-05-30 21:39:17 +0200

    Test AddNursesToRoster Method in RosterService.cs

--- backendTests/Service/InMemoryRosterServiceTests.cs
+++ backendTests/Service/InMemoryRosterServiceTests.cs
@@ -343,6 +343,46 @@ public class InMemoryRosterServiceTests
         Assert.Null(newRosterItem.Warning);
 
     }
+    
+    [Fact]
+    public async void RosterService_AddNursesToRoster_EmployeeNull()
+    {
+        // Arrange
+        var dbContext = await Context.GetDbContext();
+        RosterService rosterService = new RosterService(dbContext);
+        
+        List<Employee> availableForShift = new List<Employee>();
+        DateTime currentDay = DateTime.Now;
+        Shift shift = dbContext.Shifts.First();
+        List<EmployeeType> employeeTypes = dbContext.EmployeeTypes.ToList();
+        
+        // Act
+        rosterService.AddNursesToRoster(availableForShift, currentDay, shift, employeeTypes);
+        Roster newRosterItem = dbContext.Rosters.Last();
+        
+        // Assert
+        Assert.NotNull(newRosterItem.Warning);
+    }
+    
+    [Fact]
+    public async void RosterService_AddNursesToRoster_EmployeeNotNull()
+    {
+        // Arrange
+        var dbContext = await Context.GetDbContext();
+        RosterService rosterService = new RosterService(dbContext);
+        
+        List<Employee> availableForShift = dbContext.Employees.ToList();
+        DateTime currentDay = DateTime.Now;
+        Shift shift = dbContext.Shifts.First();
+        List<EmployeeType> employeeTypes = dbContext.EmployeeTypes.ToList();
+        
+        // Act
+        rosterService.AddNursesToRoster(availableForShift, currentDay, shift, employeeTypes);
+        Roster newRosterItem = dbContext.Rosters.Last();
+        
+        // Assert
+        Assert.Null(newRosterItem.Warning);
+    }
 
     public Roster NewRosterItem(DataContext dbContext, Employee employee)
           {

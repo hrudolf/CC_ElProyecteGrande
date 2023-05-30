b186cda4569dfecbd801372121f68eeca5d8afbe
hrudolf <h.rudolf@gmail.com>
2023-05-30 22:06:39 +0200

    Add PasswordServiceTest.cs

---
+++ backendTests/Service/PasswordServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using backend.Service;
+
+namespace backendTests.Service;
+
+public class PasswordServiceTest
+{
+    [Fact]
+    public void HashHasCorrectFormat()
+    {
+        //Act
+        var password = "password";
+        var hashedPassword = PasswordService.HashPass(password);
+        
+        var expectedLength = 60;
+
+        Assert.Equal(expectedLength, hashedPassword.Length);
+    }
+    
+    [Fact]
+    public void PasswordAndHashDiffer()
+    {
+        //Act
+        var password = "password";
+        var hashedPassword = PasswordService.HashPass(password);
+
+        Assert.NotEqual(password, hashedPassword);
+    }
+    
+    [Fact]
+    public void PasswordValidationSuccess()
+    {
+        //Act
+        var password = "password";
+        var hashedPassword = PasswordService.HashPass(password);
+        bool samePassword = PasswordService.VerifyPass(hashedPassword, password);
+
+        Assert.True(samePassword);
+    }
+}

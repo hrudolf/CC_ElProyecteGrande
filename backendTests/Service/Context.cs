18cd31e1ddc31b63feabfc58e3dcb0f517d36aa9
Gergely Papp <gregpappdev@gmail.com>
2023-05-30 16:53:57 +0200

    Test Create method in RosterService.cs

---
+++ backendTests/Service/Context.cs
@@ -0,0 +1,20 @@
+﻿using backend.Database;
+using backend.Database.Seed;
+using Microsoft.EntityFrameworkCore;
+
+namespace backendTests.Service;
+
+public class Context
+{
+    public static async Task<DataContext> GetDbContext()
+    {
+        var options = new DbContextOptionsBuilder<DataContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+        var databaseContext = new DataContext(options);
+        databaseContext.Database.EnsureCreated();
+        DataSeed dataSeed = new DataSeed(databaseContext);
+        dataSeed.CreateAll();
+        return databaseContext;
+    }
+}
---
+++ backendTests/Service/InMemoryRosterServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using backend.Model;
+using backend.Service;
+
+namespace backendTests.Service;
+
+public class InMemoryRosterServiceTests
+{
+    [Fact]
+    public async void RosterService_Create_ReturnRoster()
+    {
+        var dbContext = await Context.GetDbContext();
+        
+        // Arrange
+        var newRosterItem = new Roster()
+        {
+            Date = DateTime.Now,
+            Shift = dbContext.Shifts.FirstOrDefault(),
+            Attendance = false,
+            Employee = dbContext.Employees.FirstOrDefault(),
+            Warning = null
+        };
+
+        RosterService rosterService = new RosterService(dbContext);
+        int numberOfRosterItems = dbContext.Rosters.Count();
+
+        // Act
+        rosterService.Create(newRosterItem);
+        int rosterItemsAfterCreate = dbContext.Rosters.Count();
+
+        // Assert
+        Assert.Equal(numberOfRosterItems + 1, rosterItemsAfterCreate);
+    }
+}
